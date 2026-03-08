8ba02690cab89533e19a3f8f99b18826a7a9b571
switchlivefr <switch.live.fr@gmail.com>
2026-03-08 10:38:01 +0100

    Add files via upload

---
+++ functions/resa.js
@@ -0,0 +1,6 @@
+export async function onRequest() {
+  return Response.redirect(
+    'https://www.google.com/maps/reserve/v/dine/c/pM-d3FI8_go?source=pa&opi=89978449&hl=fr-FR&gei=K_2pabbMCJXInsEPm_S04Q8&sourceurl=https://www.google.com/search?client%3Dms-android-oppo-rvo3%26sca_esv%3D404b834684c619cd%26hl%3Dfr-FR%26cs%3D1%26kgmid%3D/g/11f00nydcl%26q%3DPro%2BRace%2BCafe%26shndl%3D30%26source%3Dsh/x/loc/act/m1/4%26kgs%3D53f4d70db6cab96c%26shem%3Dshrtsdl',
+    302
+  );
+}
