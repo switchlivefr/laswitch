db0efd12dcf979c98d41a7d1373b901bca303cf9
switchlivefr <switch.live.fr@gmail.com>
2026-03-12 15:22:51 +0100

    Add files via upload

--- _worker.js
+++ _worker.js
@@ -1,5 +1,20 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
   const destination = 'https://laswitch.net' + url.pathname + url.search;
-  return Response.redirect(destination, 301);
+  const html = `<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="refresh" content="0;url=${destination}">
+  <link rel="canonical" href="${destination}">
+  <title>La SWiTCH</title>
+</head>
+<body>
+  <p>Redirection en cours... <a href="${destination}">Cliquez ici</a></p>
+</body>
+</html>`;
+  return new Response(html, {
+    status: 200,
+    headers: { 'Content-Type': 'text/html;charset=UTF-8' }
+  });
 }
